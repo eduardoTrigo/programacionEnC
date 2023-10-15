f9f6e0148920134204da3c40c84ee1b92f696fb7
Eduardo Trigo <eduatrigo@gmail.com>
2023-10-15 00:09:43 -0300

    modificando

--- tp-4/punto2-VocalesB.c
+++ tp-4/punto2-VocalesB.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     char cc;
-    int cantPalabras=0, pasaPalabra=0;
+    int cantPalabras = 0, pasaPalabra = 0;
     printf("ingrese una frase terminada en .\n");
     fflush(stdin);
-    scanf("%c",&cc );
+    scanf("%c", &cc);
     while (cc != '.')
     {
-       while (pasaPalabra == 0)
-       {
-        switch (cc)
+        while (pasaPalabra == 0)
         {
-        case 'a':
-        case 'A':
-        case 'e':
-        case 'E':
-        case 'o':
-        case 'O':
-            cantPalabras += 1;
-            pasaPalabra = 1;
-            break;
-        default:            
-            break;
+            switch (cc)
+            {
+            case 'a':
+            case 'A':
+            case 'e':
+            case 'E':
+            case 'o':
+            case 'O':
+                cantPalabras += 1;
+                pasaPalabra = 1;
+                break;
+            default:
+                break;
+            }
+            scanf("%c", &cc);
         }
-       }
-       scanf("%c",&cc);
-       if ( cc == ' ')
-       {
-        pasaPalabra = 0;
-        scanf("%c",&cc);
-       }
        
+            if (cc == ' ')
+            {
+                pasaPalabra = 0;
+                scanf("%c", &cc);
+            }
+                
+            if (cc != '.')
+            {
+                scanf("%c", &cc);
+            }                
     }
 
-    printf("cantidad de palabras con vocales abiertas:%d\n",cantPalabras);
-    
+    printf("cantidad de palabras con vocales abiertas:%d", cantPalabras);
+
     return 0;
 }
